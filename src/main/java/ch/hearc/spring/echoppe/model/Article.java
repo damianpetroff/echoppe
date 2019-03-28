36bfb9752361b350a3132c088e00939a2626ebb6
damianpetroff <damian.petroff@he-arc.ch>
2019-03-28 10:09:41 +0100

    UnitTest for Article

--- src/main/java/ch/hearc/spring/echoppe/model/Article.java
+++ src/main/java/ch/hearc/spring/echoppe/model/Article.java
@@ -48,6 +48,10 @@ public class Article {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public Long getId() {
+		return id;
+	}
+	
 	public String getName() {
 		return name;
 	}
---
+++ src/test/java/ch/hearc/spring/echoppe/repository/ArticleRepositoryTest.java
@@ -0,0 +1,45 @@
+package ch.hearc.spring.echoppe.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ch.hearc.spring.echoppe.model.Article;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ArticleRepositoryTest {
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@Test
+	public void givenArticle_whenPersistArticle_theArticleIsPersisted() {
+		Article article = new Article();
+		article.setPrice(BigDecimal.ONE);
+		article.setName("test article");
+		
+		entityManager.persist(article);
+		entityManager.flush();
+		
+		Optional<Article> articleRecherche = articleRepository.findById(article.getId());
+		
+		assertTrue(articleRecherche.isPresent());
+		assertTrue(articleRecherche.get().getId().equals(article.getId()));
+		
+		assertThat(articleRecherche.get()).isNotNull();
+	}
+
+}
