3445a39d4788e672a2214e45ba14429af9182be0
quentin.michel <quentin.michel@he-arc.ch>
2019-04-24 15:45:38 +0200

    home pimpage

--- src/main/java/ch/hearc/spring/echoppe/controller/HomeController.java
+++ src/main/java/ch/hearc/spring/echoppe/controller/HomeController.java
@@ -1,14 +1,25 @@
 package ch.hearc.spring.echoppe.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ch.hearc.spring.echoppe.model.Article;
+import ch.hearc.spring.echoppe.repository.ArticleRepository;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private ArticleRepository arepo;
+	
 	@Value("${accueil.message:test}")
 	private String message;
 
@@ -18,6 +29,19 @@ public class HomeController {
 	@GetMapping("/")
 	public String home(Map<String, Object> model) {
 		model.put("page", "Home");
+		Random rand = new Random();
+	    List<Article> articles = arepo.findAll();
+	    List<Article> randomArticles = new ArrayList();
+	 
+	    int numberOfElements = 4;
+	 
+	    for (int i = 0; i < numberOfElements; i++) {
+	        int randomIndex = rand.nextInt(articles.size());
+	        randomArticles.add(articles.get(randomIndex));
+	    }
+	    
+	    model.put("randomArticles", randomArticles);
+	    	    
 		return "home";
 	}
 
--- src/main/java/ch/hearc/spring/echoppe/repository/ArticleRepository.java
+++ src/main/java/ch/hearc/spring/echoppe/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package ch.hearc.spring.echoppe.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -11,6 +13,7 @@ import ch.hearc.spring.echoppe.model.Article;
 public interface ArticleRepository extends PagingAndSortingRepository<Article, Long> {
 	Article findById(long id);
 	Article findByName(String name);
+	List<Article> findAll();
 	Page<Article> findAllByNameIgnoreCaseContaining(String name, Pageable pageable);
 	Page<Article> findAllByCategoryNameIgnoreCaseContaining(String query, Pageable page);
 	Page<Article> findAllByPriceLessThan(Double double1, Pageable page);
